543ad605067e1863d0f7dc4c2cd15f14e7cf8e97
doc97 <daniel.riissanen@gmail.com>
2019-03-08 16:56:42 +0200

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const middleware = require('./utils/middleware')
 const emissionsRouter = require('./controllers/emissions')
 
 app.use(express.static('build'))
+app.use(cors())
 app.use(middleware.requestLogger)
 
 app.use('/api/emissions', emissionsRouter)
