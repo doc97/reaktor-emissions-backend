a5975c12625e165b7dcdeb017170cfc74e40cb07
doc97 <daniel.riissanen@gmail.com>
2019-03-10 15:59:45 +0200

    Add prebuilt frontend files

---
+++ build/precache-manifest.c4bcf837dfe9de82900a4a26f2dcac6d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e07b07559a5fdc1f96d",
+    "url": "/static/js/main.f042aab0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6bfc7856240b6f611b12",
+    "url": "/static/js/2.67aa76b0.chunk.js"
+  },
+  {
+    "revision": "3beb6e774e0af1f77effd56c69d4d064",
+    "url": "/index.html"
+  }
+];
