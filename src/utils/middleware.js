04f09375a7941dc599649efb8465705f91002162
doc97 <daniel.riissanen@gmail.com>
2019-03-11 19:44:22 +0200

    Update request logger middleware

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -3,10 +3,6 @@ const logger = require('./logger')
 const requestLogger = (request, response, next) => {
   logger.info('Method: ', request.method)
   logger.info('Path:   ', request.path)
-  logger.info('Body:   ', request.body)
-  if (request.token) {
-    logger.info('Token:  ', request.token)
-  }
   logger.info('---')
   next()
 }
