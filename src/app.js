b1788e82a20409c4bcd7e7ecdc8d5f92a13a48a2
doc97 <daniel.riissanen@gmail.com>
2019-03-11 20:41:11 +0200

    Fix client-side routing bug

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const app = express()
 const middleware = require('./utils/middleware')
@@ -9,6 +10,9 @@ app.use(cors())
 app.use(middleware.requestLogger)
 
 app.use('/api/emissions', emissionsRouter)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
